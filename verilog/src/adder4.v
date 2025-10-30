14c264a10eb1fb8f2c679d60c8c64ea51b2e161f
lucasatwrk <112657074+lucasatwrk@users.noreply.github.com>
2025-10-30 15:51:08 +0000

    Fix HDL code and use verilator built-in cmake function

--- verilog/src/adder4.v
+++ verilog/src/adder4.v
@@ -9,10 +9,8 @@ module adder4(
 
     wire [3:1] c;
 
-    fulladder fa1(a[0],b[0], c_in, sum[0], c[1]) ;
-    fulladder fa2(a[1],b[1], c[1], sum[1], c[2]) ;
-    fulladder fa3(a[2],b[2], c[2], sum[2], c[3]) ;
-    fulladder fa4(a[3],b[3], c[3], sum[3], c[4]) ;
-;
-
+    fulladder fa1(a[0], b[0], c_in, sum[0], c[1]) ;
+    fulladder fa2(a[1], b[1], c[1], sum[1], c[2]) ;
+    fulladder fa3(a[2], b[2], c[2], sum[2], c[3]) ;
+    fulladder fa4(a[3], b[3], c[3], sum[3], c_out) ;
 endmodule
